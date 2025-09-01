514fb8632e4130a689938c6f707f0c6fb5ba3135
Andrei-KV <a.v.kovalev9@gmail.com>
2025-09-01 19:48:04 +0300

    Add: header in base.html

--- places/urls.py
+++ places/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('add/', views.add_place, name='add_place'),
     path('edit/<int:place_id>/', views.edit_place, name='edit_place'),
     path('register/', views.register, name='register'), 
+
+
+    path('', views.home_page, name='about'),
+    path('', views.home_page, name='contacts'),
+    path('', views.home_page, name='categories'),
+
 ]
